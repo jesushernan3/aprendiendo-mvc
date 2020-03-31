8854c3c85d44318f5a8cf8c91426b40bc508c944
jesus hernan <jesushernan@gmail.com>
2020-03-31 11:28:00 -0300

    funciones de encriptacion

--- core/configAPP.php
+++ core/configAPP.php
@@ -3,3 +3,9 @@
   const DB="";
   const USER="";
   const PASS="";
+
+  const SGDB="mysql:host=".SERVER.";dbname="DB;
+
+  const METHOD="AES_256_CBC";
+  const SECRET_KEY='$BP@2017';
+  const SECRET_IV='101721';
