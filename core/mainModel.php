cb8b21a9add487d3eb9697a0af8326405bfbea35
jesus hernan <jesushernan@gmail.com>
2020-03-31 13:54:09 -0300

    Funcion para mostrar alertas en mainModelo.php

--- core/mainModel.php
+++ core/mainModel.php
@@ -1,25 +1,26 @@
 <?php
   
     
-  if ($peticionAjax) {
+  if ($peticionAjax){
     	require_once '../core/configAPP.php';
-    } else {
+    } else{
     	require_once './core/configAPP.php';
     }
 
-  class mainModel {
+  class mainModel{
     
-    protected function conectar() {
+    protected function conectar(){
 			$enlace = new PDO(SGBD, USER, PASS);
 			return $enlace;    	
     }
 
-    protected function ejecutar_consulta_simple($consulta) {
+    protected function ejecutar_consulta_simple($consulta){
     	$respuesta = self::conectar()->prepare($consulta);
     	$respuesta -> execute();
     	return $respuesta;
     }
 
+    // Funciones para encriptar contraseñas
     public static function encryption($string){
 			$output=FALSE;
 			$key=hash('sha256', SECRET_KEY);
@@ -34,6 +35,88 @@
 			$output=openssl_decrypt(base64_decode($string), METHOD, $key, 0, $iv);
 			return $output;
 		}
+
+		// Funcion para generar text aleatorio para sumarle al uri
+		protected function generar_codigo_aleatorio($letra, $longitud, $num){
+			for($i=1; $i<=$longitud; $i++){
+				$numero = rand(0,9);
+				$letra.= $numero;
+			}
+			return $letra.$num;
+		}
+
+		// Limpiar texto de formularios
+		protected function limpiar_cadena($cadena){
+			$cadena=trim($cadena);
+			$cadena=stripslashes($cadena);
+			$cadena=str_ireplace("<script>", "", $cadena);			
+			$cadena=str_ireplace("</script>", "", $cadena);			
+			$cadena=str_ireplace("<script src>", "", $cadena);			
+			$cadena=str_ireplace("SELECT * FROM", "", $cadena);
+			$cadena=str_ireplace("DELETE FROM", "", $cadena);			
+			$cadena=str_ireplace("INSERT INTO", "", $cadena);			
+			$cadena=str_ireplace("--", "", $cadena);			
+			$cadena=str_ireplace("^", "", $cadena);			
+			$cadena=str_ireplace("[", "", $cadena);			
+			$cadena=str_ireplace("]", "", $cadena);			
+			$cadena=str_ireplace("==", "", $cadena);
+			$cadena=str_ireplace(";", "", $cadena);
+			return $cadena;
+		}
+
+		// Maneja mensajes de sistemas
+		protected function sweet_alert($datos){
+			if ($datos['Alerta']=="simple"){
+							$alerta="
+								<script>
+									swal(
+									  title: '".$datos.['Titulo']"',
+									  text: '".$datos.['Texto']"',
+									  '".$datos.['Tipo']"',
+									);
+									</script>
+
+							";
+			}elseif ($datos.['Alerta']=='recargar'){
+							$alerta="
+								<script>
+									Swal({
+									  title: '".$datos.['Titulo']"',
+									  text: '".$datos.['Text']"',
+									  type: '".$datos.['Tipo']",
+									  confirmButtonText: 'Aceptar'
+									}).then(function (){
+										location.reload();
+										}
+
+									});
+									</script>
+
+							";
+			}elseif($datos.['Alerta']=='limpiar'){
+							$alerta="
+								<script>
+									Swal({
+									  title: '".$datos.['Titulo']"',
+									  text: '".$datos.['Text']"',
+									  type: '".$datos.['Tipo']",
+									  confirmButtonText: 'Aceptar'
+									}).then(function (){
+											$('.FormularioAjax')[0].reset();
+										}
+									});
+									</script>
+
+							";
+			
+
+			}
+
+									
+		}
+
+
+
 
   }
   
