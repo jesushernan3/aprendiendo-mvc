466606795b445561cd356d3a326fa9a5685ed4c7
Jesus Hernan <jesushernan3@gmail.com>
2020-03-30 23:59:29 -0300

    me falto search

---
+++ core/configAPP.php
@@ -0,0 +1,5 @@
+<?php
+  const SERVER="";
+  const DB="";
+  const USER="";
+  const PASS="";
--- vistas/plantilla.php
+++ vistas/plantilla.php
@@ -1,3 +1,7 @@
+<?php
+	session_start();
+	$peticionAjax=false;
+?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
